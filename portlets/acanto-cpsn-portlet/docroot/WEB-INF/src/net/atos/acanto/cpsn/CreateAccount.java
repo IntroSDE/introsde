13ab356d55b2a0793f3d3f940a98e57b19988a2a
Ingo Brauckhoff <ingo.brauckhoff@atos.net>
2016-06-02 17:20:56 +0100

    updated liferay-plugin-package.properties

---
+++ portlets/acanto-cpsn-portlet/docroot/WEB-INF/src/net/atos/acanto/cpsn/CreateAccount.java
@@ -0,0 +1,11 @@
+package net.atos.acanto.cpsn;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class CreateAccount
+ */
+public class CreateAccount extends MVCPortlet {
+ 
+
+}
