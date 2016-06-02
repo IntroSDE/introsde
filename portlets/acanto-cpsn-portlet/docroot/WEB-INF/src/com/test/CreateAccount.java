3a287503974d9ad9a37ec691766f4006b162068c
Ingo Brauckhoff <ingo.brauckhoff@atos.net>
2016-06-02 16:51:41 +0100

    - removed faulty portlet project (acanto-create_user-portlet) - added new project "Acanto CPSN" - added new portlet to project Acanto CPSN: "Create Account"

---
+++ portlets/acanto-cpsn-portlet/docroot/WEB-INF/src/com/test/CreateAccount.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class CreateAccount
+ */
+public class CreateAccount extends MVCPortlet {
+ 
+
+}
